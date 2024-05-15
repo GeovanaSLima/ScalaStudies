b0779ff0e6c13d8951e39d189efb6d62c5d413c8
geovana <geovanasslima@gmail.com>
2024-05-14 23:10:34 -0300

    SquareRoot Function

--- Functional Programming Specialization/Course 1/Week 1/Assignment 1/src/main/scala/example/SquareRoot.scala
+++ Functional Programming Specialization/Course 1/Week 1/Assignment 1/src/main/scala/example/SquareRoot.scala
@@ -1,16 +1,16 @@
 package example
 
-def sqrIter(guess: Double, x: Double): Double = {
-  def improve(guess: Double, x: Double) =
-    (guess + x/guess) / 2
+def sqrt(x: Double) = {
+  def sqrtIter(guess: Double): Double =
+    if isGoodEnough(guess) then guess
+    else sqrtIter(improve(guess))
 
-  def isGoodEnough(guess: Double, x: Double) =
-    abs(guess * guess - x) < 0.001
+  def improve(guess: Double) =
+    (guess + x / guess) / 2
 
-  if isGoodEnough(guess, x) then guess
-  else sqrIter(improve(guess, x), x)
+  def isGoodEnough(guess: Double) =
+    abs(square(guess) - x) < 0.001
+
+  sqrtIter(1.0)
 
 }
-
-
-
--- Functional Programming Specialization/Course 1/Week 1/Assignment 1/src/test/scala/example/SquareRootSuite.scala
+++ Functional Programming Specialization/Course 1/Week 1/Assignment 1/src/test/scala/example/SquareRootSuite.scala
@@ -1,10 +1,12 @@
 package example
 
-import SquareRoot.*
-
 class SquareRootSuite extends munit.FunSuite {
 
   test("square root of 4 is 2") {
-    assert(sqrIter(1, ))
+    assert(sqrtIter(1, 4) == 2)
+  }
+
+  test("square root of 4 is 2") {
+    assert(sqrt(4) == 2)
   }
 }

f690a28d97ef68f9513078580e34cac16fc180f5
geovana <geovanasslima@gmail.com>
2024-05-16 21:41:53 -0300

    Adding Solutions for Exercise 2 and 3 of Assignment

--- Functional Programming Specialization/Course 1/Week 1/Assignment 2/src/main/scala/recfun/RecFun.scala
+++ Functional Programming Specialization/Course 1/Week 1/Assignment 2/src/main/scala/recfun/RecFun.scala
@@ -14,7 +14,6 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 1
    */
-//  @tailrec
   def pascal(c: Int, r: Int): Int = {
     if (c == 0 || c == r) 1
     else (pascal(c - 1, r - 1) + pascal(c, r - 1))
@@ -23,9 +22,60 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def balanceHelper(chars: List[Char], openCount: Int): Boolean = {
+      if (chars.isEmpty) {
+        openCount == 0
+      } else {
+        val head = chars.head
+        val newOpenCount =
+          if (head == '(') openCount + 1
+          else if (head == ')') openCount - 1
+          else openCount
+
+        if (newOpenCount < 0) false
+        else balanceHelper(chars.tail, newOpenCount)
+      }
+    }
+
+    balanceHelper(chars, 0)
+  }
+
+  // Testing balance function
+  @main def runBalance(): Unit = {
+    val testString = "this is a (test) )(".toList
+    val second = "(if (zero? x) max (/ 1 x))".toList
+    val third = ":-)".toList
+    val fourth = "".toList
+
+    println(s"Parenthesis Balancing: ${balance(testString)}")
+    println(s"Parenthesis Balancing: ${balance(second)}")
+    println(s"Parenthesis Balancing: ${balance(third)}")
+    println(s"Parenthesis Balancing: ${balance(fourth)}")
+//    println(testString)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def changeHelper(remaining: Int, availableCoins: List[Int]): Int = {
+      if (remaining == 0) {
+        1
+      } else if (remaining < 0 || availableCoins.isEmpty) {
+        0
+      } else {
+        changeHelper(remaining - availableCoins.head, availableCoins) +
+          changeHelper(remaining, availableCoins.tail)
+      }
+    }
+
+    if (money == 0) 0 else changeHelper(money, coins)
+  }
+
+  @main def testCountChange(): Unit = {
+    println(s"Money: 4\n Coins: [1, 2]\n Total Combinations: ${countChange(4, List(1, 2))}")
+    println(s"Money: 6\n Coins: [1, 2, 3]\n Total Combinations: ${countChange(6, List(1, 2, 3))}")
+    println(s"Money: 20\n Coins: [1, 2, 3, 4]\n Total Combinations: ${countChange(20, List(1, 2, 3, 4))}")
+  }
